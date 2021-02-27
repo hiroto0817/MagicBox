c14050a0ae7eacbc34de2293ed9f0d746bc4bcd0
LAPTOP-JAPHMNR6\owner <i.hiroto0817@gmail.com>
2021-02-27 18:45:53 +0900

    サーボ5個の動作を追加

--- magicbox/magicbox.ino
+++ magicbox/magicbox.ino
@@ -1,9 +1,44 @@
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
+
+int waittime = 1000;
+
 void setup() {
-  // put your setup code here, to run once:
-aaaaaa
+  servo1.attach(9);
+  servo2.attach(10);
+  servo3.attach(11);
+  servo4.attach(12);
+  servo5.attach(13);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  servo1.write(0);
+  servo2.write(0);
+  servo3.write(0);
+  servo4.write(0);
+  servo5.write(0);
+  
+  delay(waittime);
+  
+  servo1.write(90);
+  servo2.write(90);
+  servo3.write(90);
+  servo4.write(90);
+  servo5.write(90);
+  
+  delay(waittime);
+  
+  servo1.write(180);
+  servo2.write(180);
+  servo3.write(180);
+  servo4.write(180);
+  servo5.write(180);
+  
+  delay(waittime);
+  
 }
