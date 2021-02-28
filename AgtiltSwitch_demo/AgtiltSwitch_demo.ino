8d1f8c73f75124c329774691b033f4e2e09921bc
LAPTOP-JAPHMNR6\owner <i.hiroto0817@gmail.com>
2021-02-28 12:58:13 +0900

    振動センサーでサーボモータを動作させた

--- AgtiltSwitch_demo/AgtiltSwitch_demo.ino
+++ AgtiltSwitch_demo/AgtiltSwitch_demo.ino
@@ -1,22 +1,24 @@
 int led = 13;
-int Agtilt = 3;
+int SwitchPin = 10;
 int val;
 
 void setup() {
   pinMode(led, OUTPUT);
-  pinMode(Agtilt, INPUT);
+  pinMode(SwitchPin, INPUT);
   
 }
 
 void loop() {
-  val = digitalRead(Agtilt);
+  val = digitalRead(SwitchPin);
+  
   if(val == HIGH)
   {
-    digitalRead(led, HIGH);
-  }
-  else
-  {
     digitalWrite(led, LOW);
+  }
+  else if(val == LOW)
+  {
+    digitalWrite(led, HIGH);
+    delay(10000);
   }
 
 }
--- magicbox/magicbox.ino
+++ magicbox/magicbox.ino
@@ -6,7 +6,9 @@ Servo servo3;
 Servo servo4;
 Servo servo5;
 
+int SwitchPin = 8;
 int waittime = 1000;
+int val;
 
 void setup() {
   servo1.attach(9);
@@ -17,28 +19,33 @@ void setup() {
 }
 
 void loop() {
-  servo1.write(0);
-  servo2.write(0);
-  servo3.write(0);
-  servo4.write(0);
-  servo5.write(0);
+  val = digitalRead(SwitchPin);
   
-  delay(waittime);
-  
-  servo1.write(90);
-  servo2.write(90);
-  servo3.write(90);
-  servo4.write(90);
-  servo5.write(90);
-  
-  delay(waittime);
-  
-  servo1.write(180);
-  servo2.write(180);
-  servo3.write(180);
-  servo4.write(180);
-  servo5.write(180);
-  
-  delay(waittime);
+  if(val == LOW)
+  {
+    servo1.write(0);
+    servo2.write(0);
+    servo3.write(0);
+    servo4.write(0);
+    servo5.write(0);
+    
+    delay(waittime);
+    
+    servo1.write(90);
+    servo2.write(90);
+    servo3.write(90);
+    servo4.write(90);
+    servo5.write(90);
+    
+    delay(waittime);
+    
+    servo1.write(180);
+    servo2.write(180);
+    servo3.write(180);
+    servo4.write(180);
+    servo5.write(180);
+    
+    delay(waittime);
+  }
   
 }
